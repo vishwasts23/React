dc05a129b17d3b71a33e899193a988827f86c6a3
vishwasts23 <vishwasts42@gmail.com>
2026-05-25 17:17:40 +0530

    First React self introduction project

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import SelfIntroduction from "./SelfIntroduction";
+import "./App.css";
+
+export default function App() {
+  return (
+    <>
+      <SelfIntroduction />
+    </>
+  );
+}
---
+++ src/SelfIntroduction.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+export default function SelfIntroduction() {
+  return (
+    <div className="container">
+      <h2>My Self Introduction</h2>
+
+      <p><b>Name:</b> Vishwas T S</p>
+
+      <p><b>Education:</b> BE in Computer Science</p>
+
+      <p><b>Skills:</b> HTML, CSS, JavaScript, Testing</p>
+
+      <p><b>Location:</b> Karnataka</p>
+
+      <p><b>Hobbies:</b> Gym and Bike Riding</p>
+
+      <p><b>Career Goal:</b>To become a successful Software
+          Engineer and grow in the IT industry.</p>
+    </div>
+  );
+}
